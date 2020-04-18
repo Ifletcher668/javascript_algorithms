917506396ffa536ebe206805daec80663fa0fcf8
Isiah Fletcher <Isiah@Colleen-Fletchers-MacBook.local>
2020-04-18 08:41:44 -0600

    First commit

---
+++ linked_lists/sll_practice.js
@@ -0,0 +1,258 @@
+
+
+
+////     1. SList: Add Back
+////     2. seedFromArr: SList: Convert Array to SList
+////     3. Bonus: addBack recursive
+////     4. List: Add Front
+////     5. List: Remove Front
+/////     7. List: Contains (with & without recursion)
+/////       - check if the list contains a value
+////     8. SList: Remove Back
+////       - remove the last node from the list
+////       - bonus: return the removed val, or null if nothing was removed
+//     9. Bonus:
+//       - SList: Split on Value
+//       - splitOnVal(5) for the list (1=>3=>5=>2=>4) will change list to (1=>3), and the return value will be a new list containing (5=>2=>4)
+//     10. SList: Move Min to Front
+//     11. SList: Remove Val
+//         - remove the node with the specified value, return the removed val, or null if nothing was removed
+//     12. Bonus: 
+//         - displayPeople: in a linked list containing person objects, write a method to print a comma separated string of all the people's firstName
+
+class Node {
+    constructor(data) {
+        this.next = null;
+        this.data = data
+    }
+}
+
+class LinkedList {
+
+    constructor() {
+        this.head = null;
+    }
+
+    isEmpty() {
+        return this.head === null; 
+    };
+
+    display() {
+        if (this.head === null) {
+            console.log("An empty list")
+            return 
+        }
+        if(this.head.next === null) {
+            console.log(`List contains a single node:\n\t\tData is "${this.head.data}";\n\t\tnext value is "${this.head.next}"`);
+            return 
+        }
+        let runner = this.head;
+        let values = ""; 
+        while (runner) {
+            if (runner.next === null) {
+                values += `and '${runner.data}'.`
+                break;
+            };
+            values += `'${runner.data}', `;
+            runner = runner.next;
+        };
+        console.log(`List values are:\n\t\t${values}`);
+    };
+
+    seedFromArray(arr) {
+        // check for empty array
+        if (arr.length === 0) {
+            console.log("empty array given, could not instantiate a Singly Linked List")
+            return null
+        } else {
+            for (let i = 0; i < arr.length; i++) {
+                let node = new Node(arr[i])
+                // add to back 
+                if (this.head === null) {
+                    console.log("?")
+                    this.head = node; 
+                } else {
+                let runner = this.head;
+                while (runner.next !== null) {
+                    runner = runner.next;
+                }
+                runner.next = node; 
+                }
+            }
+        }
+    };
+
+    addToFront(val) {
+        // check if empty
+        if (this.isEmpty()) {
+            this.head = new Node(val);
+        }
+        // point val.next to this.head
+        // redirect this.head to point to val 
+        let newNode = new Node(val); 
+        newNode.next = this.head
+        this.head = newNode; 
+        return this
+    };
+
+    addToBack(val) {
+        const newNode = new Node(val);
+        if (this.head === null) {
+            this.head = newNode; 
+            // this.display();
+            // console.log(`\nFirst new Node instantiated:\n\t\tData is "${this.head.data}";\n\t\tnext value is "${this.head.next}"`); 
+            return this
+        }
+        
+        let runner = this.head; 
+        while(runner.next !== null) {
+            runner = runner.next;
+        }
+        
+        runner.next = newNode; 
+        // this.display();
+        // console.log(`\nNew Node instantiated at back: \n\t\tData is "${runner.data}";\n\t\tnext value is "${runner.next.data}"`); 
+        return this 
+    };
+    
+    addToBackRecursive(val, runner = this.head) {
+        const newNode = new Node(val);
+        // base case, where do I want the recursion to end? 
+        if (runner === null) {
+            runner = newNode; 
+            this.display();
+            return this
+        } else if (runner.next === null ) {
+            runner.next = newNode; 
+            this.display();
+            return this 
+        }
+        runner = runner.next; 
+        return this.addToBackRecursive(val, runner); 
+    }
+
+    removeFromFront() {
+        if (this.isEmpty()) {
+            return null 
+        }
+        else if (this.head.next === null) {
+            return this.head
+        }
+        else {
+            let head = this.head;
+            let temp = head.next;
+            head.next = null;
+            this.head = temp;
+            return head.data;
+        };
+    };
+
+
+    removeFromBack() {
+        if (this.isEmpty()) {
+            console.log("List is empty")
+            return null 
+        } else if (this.head.next === null) {
+            console.log("Removed the head, the only item in this list")
+            let temp = this.head.data;
+            this.head = null; 
+            return temp
+        }
+        else {
+            let runner = this.head;
+            while (runner.next.next !== null) {
+                runner = runner.next; 
+            }
+            let temp = runner.next.data; 
+            runner.next = null;
+            return temp 
+        }
+        
+
+    }
+
+    middleValue() {
+        if (this.isEmpty() || this.head.next === null || this.head.next.next === null) {
+            return null
+        }
+        else if (this.head.next.next.next === null) {
+            return this.head.next.data  
+        }
+        else {
+            let count = 0;
+            let runner = this.head;
+            while (runner !== null) {
+                count += 1;
+                runner = runner.next
+            }
+            runner = this.head; 
+            if (count % 2 === 0) {
+                count /= 2;
+                let list = [];
+                for (let i = 0; i < count; i++) {
+                    if(i + 1 === count) {
+                        break; 
+                    }
+                    runner = runner.next 
+                }
+                list.push(runner.data)
+                list.push(runner.next.data)
+                return list 
+            }
+            else {
+                count = Math.floor(count / 2);  
+                for (let i = 0; i < count; i++) {
+                    runner = runner.next 
+                }
+                return runner.data 
+            }
+
+        }
+    };
+
+    contains(val) {
+        // check for empty
+        // check for one value 
+        // run through list looking for val 
+        // what if val isn't found
+        // find val 
+        if (this.isEmpty()) {
+            return null;
+        }
+        if (this.head.next === null) {
+            if (this.head === val) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        let runner = this.head 
+        while(runner !== null) {
+            if (runner.data === val) {
+                console.log("value found")
+                return true; 
+            }
+            runner = runner.next;
+        }
+        console.log("value not found")
+        return false 
+
+    }
+
+
+
+};
+
+const list = new LinkedList(); 
+const array = [1,2,3,4,5];
+
+// list.addToBack(100).addToBack(20).addToBack("string");
+// list.display();
+list.seedFromArray(array);
+// list.addToFront(100).addToFront("string")
+// list.addToBackRecursive(200).addToBackRecursive(300).addToBackRecursive(3112300).addToBackRecursive("3000").addToBack([1,2,3]).addToBack(100); 
+// list.removeFromBack();
+list.display();
+
+
+
---
+++ stacks_and_queues/neils_examples.js
@@ -0,0 +1,431 @@
+  
+/* 
+  w2d1
+    - A stack is a LIFO (Last in First Out) data structure
+    - create a class to represent a stack using an array & another class for a stack using a singly linked list
+      - create methods for each: push, pop, isEmpty, size, peek (return top item without removing)
+  w2d2
+    - A queue is a FIFO (First in First Out) data structure
+    - create a class to represent a queue using an array & another class for a queue using a singly linked list
+    - create methods for each classes: enqueue (add item), dequeue (remove and return item), isEmpty, size, front (return first item without removing)
+  w2d3
+    - compareQueues
+      - write a method on the Queue class that will be given another Queue, determine if both queues are equal (all same items in same order)
+        - DO NOT manually loop over the queue items, use the provided methods (enqueue and dequeue) to do the comparison
+        - return whether or not they are equal and restore both queues to their original order
+  w2d4
+    - sumOfHalvesEqual
+      - create a method on the Queue class that returns whether or not the sum of the first half of the queue is equal to the sum of the second half
+      - DO NOT manually index the queue items, only use the provided queue methods, use no additional arrays or objects for storage
+      - restore the queue to it's original state before returning
+    - nextQueue
+      - Design a Queue class that automatically sends every 3rd dequeued person object to a next queue that can be specified
+      - Imagine a security queue where every 3rd person is randomly sent to an additional security queue
+  w2d5
+    - priorityQueue (create enqueue and dequeue methods)
+      - design a new PriorityQueue class where the queue maintains an ascending order when items are added based on queue item's provided priority integer value
+    - BONUS: see processPallets method on queue class
+*/
+
+class Stack {
+    constructor(items = []) {
+      this.items = items;
+    }
+  
+    push(item) {
+      this.items.push(item);
+    }
+  
+    pop() {
+      if (this.isEmpty()) {
+        return "Underflow";
+      }
+  
+      return this.items.pop();
+    }
+  
+    peek() {
+      // aka top
+      return this.items[this.items.length - 1];
+    }
+  
+    isEmpty() {
+      return this.items.length === 0;
+    }
+  
+    size() {
+      return this.items.length;
+    }
+  
+    print() {
+      const str = this.items.join(" ");
+      console.log(str);
+      return str;
+    }
+  }
+  
+  class Node {
+    constructor(data) {
+      this.data = data;
+      this.next = null;
+    }
+  }
+  
+  // can add to back and remove from back to maintain a LIFO pattern
+  // OR can add to front and remove from front, still maintains a LIFO pattern but is faster
+  // because it requires no looping to the end of the linked list
+  class SLStack {
+    constructor() {
+      this.head = null;
+    }
+  
+    // add to top (add new head)
+    push(val) {
+      const newNode = new Node(val);
+  
+      if (this.head === null) {
+        this.head = newNode;
+      } else {
+        newNode.next = this.head;
+        this.head = newNode;
+      }
+    }
+  
+    // remove from top, (remove head)
+    pop() {
+      if (this.head === null) {
+        return null;
+      }
+  
+      const removed = this.head;
+      this.head = this.head.next;
+  
+      return removed.data;
+    }
+  
+    // aka top
+    peek() {
+      if (this.head === null) {
+        return null;
+      }
+      return this.head.data;
+    }
+  
+    contains(val) {
+      let runner = this.head;
+  
+      while (runner) {
+        if (runner.data === val) {
+          return true;
+        }
+        runner = runner.next;
+      }
+      return false;
+    }
+  
+    isEmpty() {
+      return this.head === null;
+    }
+  
+    size() {
+      let len = 0;
+      let runner = this.head;
+  
+      while (runner) {
+        len += 1;
+        runner = runner.next;
+      }
+      return len;
+    }
+  
+    print() {
+      let runner = this.head;
+      let vals = "";
+  
+      while (runner) {
+        vals += `${runner.data}${runner.next ? ", " : ""}`;
+        runner = runner.next;
+      }
+      console.log(vals);
+      return vals;
+    }
+  }
+  
+  class Queue {
+    constructor(items = []) {
+      this.items = items;
+    }
+  
+    enqueue(item) {
+      this.items.push(item);
+    }
+  
+    dequeue() {
+      if (this.isEmpty()) {
+        return "Underflow";
+      }
+  
+      return this.items.shift();
+    }
+  
+    front() {
+      if (this.isEmpty()) {
+        return "Queue empty";
+      }
+  
+      return this.items[0];
+    }
+  
+    isEmpty() {
+      return this.items.length === 0;
+    }
+  
+    size() {
+      return this.items.length;
+    }
+  
+    print() {
+      const str = this.items.join(" ");
+      console.log(str);
+      return str;
+    }
+  
+    compareQueues(q2) {
+      if (this.size() !== q2.size()) {
+        return false;
+      }
+      let count = 0;
+      let isEqual = true;
+      const len = this.size();
+  
+      while (count < len) {
+        const dequeued1 = this.dequeue();
+        const dequeued2 = q2.dequeue();
+  
+        if (dequeued1 !== dequeued2) {
+          isEqual = false;
+        }
+  
+        this.enqueue(dequeued1);
+        q2.enqueue(dequeued2);
+        count++;
+      }
+  
+      return isEqual;
+    }
+  
+    isSumOfHalvesEqual() {
+      const len = this.size();
+  
+      if (len % 2 !== 0) {
+        return false;
+      }
+  
+      const halfLen = len / 2;
+      let leftSum = 0;
+      let rightSum = 0;
+      let count = 0;
+  
+      while (count < len) {
+        const dequeued = this.dequeue();
+  
+        if (count < halfLen) {
+          leftSum += dequeued;
+        } else {
+          rightSum += dequeued;
+        }
+  
+        count++;
+        this.enqueue(dequeued);
+      }
+      return leftSum === rightSum;
+    }
+  
+    /* 
+      Bonus:
+      Visualize a pallet of products, it is a grid (rows and columns, like a chess board)
+      where eatch grid space has a stack of products:
+      Process shipment of pallets that arrived, process them in the order they were received (FIFO):
+      Given a queue of pallets (this queue) write a processPallets method,
+      where a pallet is a 2d grid 
+      where the nested arrays contain stacks of product objects containing a name and price key
+      work through the queue, unload the pallets and
+      return a bill of goods containing all the names of products received and the total value of the merchandise
+      Unloading simply means removing all the items from the queues and stacks via the appropriate methods
+      Bonus: unload the products into a priority queue based on a daysUntilExpiration
+        return this priority queue along with the report.
+    */
+  
+    processPallets() {}
+  }
+  
+  class SLQueue {
+    constructor() {
+      this.head = null;
+    }
+  
+    enqueue(val) {
+      // add to back
+      const newBack = new Node(val);
+      let runner = this.head;
+  
+      if (runner === null) {
+        this.head = newBack;
+      } else {
+        while (runner.next) {
+          runner = runner.next;
+        }
+        runner.next = newBack;
+      }
+    }
+  
+    dequeue() {
+      // remove head
+      if (!this.head) {
+        return null;
+      }
+  
+      const dequeued = this.head;
+      this.head = this.head.next;
+      return dequeued.data;
+    }
+  
+    front() {
+      if (this.head === null) {
+        return null;
+      }
+      return this.head.data;
+    }
+  
+    isEmpty() {
+      return this.head === null;
+    }
+  
+    contains(val) {
+      let runner = this.head;
+  
+      while (runner) {
+        if (runner.val === val) return true;
+        runner = runner.next;
+      }
+      return false;
+    }
+  
+    size() {
+      let len = 0;
+      let runner = this.head;
+  
+      while (runner) {
+        len += 1;
+        runner = runner.next;
+      }
+      return len;
+    }
+  
+    print() {
+      let runner = this.head;
+      let vals = "";
+  
+      while (runner) {
+        vals += `${runner.val}${runner.next ? ", " : ""}`;
+        runner = runner.next;
+      }
+      console.log(vals);
+      return vals;
+    }
+  
+    seed(vals) {
+      vals.forEach((val) => this.enqueue(val));
+    }
+  }
+  
+  // students not expected to use extends, could just re-write the basic methods
+  class NextQueue extends Queue {
+    constructor(items, nextQueue) {
+      // call the constructor for Queue class
+      super(items);
+  
+      // props only added to NextQueue
+      this.dequeueCount = 0;
+      this.nextQueue = nextQueue;
+    }
+  
+    // generic, all go to next queue so this class can be used generically, not just for every 3rd dequeued item
+    dequeueToNext() {
+      if (this.isEmpty()) {
+        return "Underflow";
+      }
+  
+      // dequeue method was inherited
+      const dequeued = this.dequeue();
+      this.nextQueue.enqueue(dequeued);
+      return dequeued;
+    }
+  
+    dequeueEveryThirdToNext() {
+      if (this.isEmpty()) {
+        return "Underflow";
+      }
+      const dequeued = this.dequeue();
+      this.dequeueCount++;
+  
+      if (this.dequeueCount % 3 === 0) {
+        this.nextQueue.enqueue(dequeued);
+      }
+  
+      return dequeued;
+    }
+  
+    // more flexible, allow for the logic that determines if the item goes to the next queue to be passed in
+    // and allow a new nextQueue to be provided as well
+    dequeueToNextConditionally(sendToNextCallback, nextQ = this.nextQueue) {
+      if (this.isEmpty()) {
+        return "Underflow";
+      }
+  
+      const dequeued = this.dequeue();
+      this.dequeueCount++;
+  
+      if (sendToNextCallback.call(this, dequeued) === true) {
+        nextQ.enqueue(dequeued);
+      }
+      return dequeued;
+    }
+  }
+  
+  const initItems = [
+    { name: "person 1", flaggedBySecurity: true },
+    { name: "person 2", flaggedBySecurity: false },
+    { name: "person 3", flaggedBySecurity: false },
+    { name: "person 4", flaggedBySecurity: false },
+    { name: "person 5", flaggedBySecurity: false },
+    { name: "person 6", flaggedBySecurity: false },
+  ];
+  
+  let additionalSecurityQueue = new Queue();
+  let securityQueue = new NextQueue(initItems.slice(), additionalSecurityQueue);
+  
+  securityQueue.dequeueEveryThirdToNext();
+  securityQueue.dequeueEveryThirdToNext();
+  securityQueue.dequeueEveryThirdToNext();
+  securityQueue.dequeueEveryThirdToNext();
+  securityQueue.dequeueEveryThirdToNext();
+  securityQueue.dequeueEveryThirdToNext();
+  
+  // reset for more testing
+  additionalSecurityQueue = new Queue();
+  securityQueue = new NextQueue(initItems.slice(), additionalSecurityQueue);
+  
+  const isAdditionalSecurityNeeded = function (dequeued) {
+    if (this.dequeueCount % 3 === 0 || dequeued.flaggedBySecurity) {
+      return true;
+    }
+    return false;
+  };
+  
+  securityQueue.dequeueToNextConditionally(isAdditionalSecurityNeeded);
+  securityQueue.dequeueToNextConditionally(isAdditionalSecurityNeeded);
+  securityQueue.dequeueToNextConditionally(isAdditionalSecurityNeeded);
+  securityQueue.dequeueToNextConditionally(isAdditionalSecurityNeeded);
+  securityQueue.dequeueToNextConditionally(isAdditionalSecurityNeeded);
+  securityQueue.dequeueToNextConditionally(isAdditionalSecurityNeeded);
